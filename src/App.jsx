081e350ce9dfa984b0dd9b6453706e46ed37adcb
Eduardo <el_alienx@hotmail.com>
2021-10-14 08:22:23 +0200

    refactor candidate provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 // NPM packages
+import { useState, useCallback, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // Project files
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
+import { getCollection } from "./scripts/fireStore";
 import { useCandidates } from "./state/CandidatesProvider";
 
 export default function App() {
   // Global state
-  const { status } = useCandidates();
+  const { dispatch } = useCandidates();
+
+  // Local state
+  const [status, setStatus] = useState(0); // 0 loading, 1 loaded, 2 error
+
+  // Properties
+  const PATH = "candidates";
+
+  // Methods
+  const fetchData = useCallback(async (path) => {
+    try {
+      const candidates = await getCollection(path);
+
+      dispatch({ type: "SET_CANDIDATES", payload: candidates });
+      setStatus(1);
+    } catch {
+      setStatus(2);
+    }
+  }, []);
+
+  useEffect(() => fetchData(PATH), [fetchData]);
 
   // Component
   const Browser = (
--- src/state/CandidatesProvider.jsx
+++ src/state/CandidatesProvider.jsx
@@ -1,15 +1,8 @@
 // NPM Packages
-import {
-  useContext,
-  createContext,
-  useState,
-  useCallback,
-  useEffect,
-  useReducer,
-} from "react";
+import { useContext, createContext, useReducer } from "react";
 
 // Project files
-import { getCollection } from "../scripts/fireStore";
+
 import candidatesReducer from "./candidatesReducer";
 
 // Properties
@@ -18,27 +11,9 @@ const CandidatesContext = createContext(null);
 export function CandidatesProvider({ children }) {
   // Local state
   const [candidates, dispatch] = useReducer(candidatesReducer, []);
-  const [status, setStatus] = useState(0); // 0 loading, 1 loaded, 2 error
-
-  // Properties
-  const PATH = "candidates";
-
-  // Methods
-  const fetchData = useCallback(async (path) => {
-    try {
-      const candidates = await getCollection(path);
-
-      dispatch({ type: "SET_CANDIDATES", payload: candidates });
-      setStatus(1);
-    } catch {
-      setStatus(2);
-    }
-  }, []);
-
-  useEffect(() => fetchData(PATH), [fetchData]);
 
   return (
-    <CandidatesContext.Provider value={{ candidates, dispatch, status }}>
+    <CandidatesContext.Provider value={{ candidates, dispatch }}>
       {children}
     </CandidatesContext.Provider>
   );
